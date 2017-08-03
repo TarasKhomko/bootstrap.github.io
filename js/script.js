5021fbb66a04e5e4c56fc2aaf08f722f7246462d
TarasKhomko <taras.khomko@gmail.com>
2017-08-03 17:04:51 +0300

    slick.js + slide

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+$('ul.nav li.show').hover(function() {
+  $(this).find('.hidde').stop(true, true).delay(50).fadeIn(500);
+}, function() {
+  $(this).find('.hidde').stop(true, true).delay(50).fadeOut(500);
+});
+
+
+$('.slick').slick({
+      dots: true,
+ 
+     
+});
+
+ $(document).ready(function() {
+    $("[rel='tooltip']").tooltip();    
+ 
+    $('.thumb').hover(
+        function(){
+            $(this).find('.desk_slide').fadeIn(250); //.fadeIn(250)
+        },
+        function(){
+            $(this).find('.desk_slide').fadeOut(250); //.fadeOut(205)
+        }
+    ); 
+ });
